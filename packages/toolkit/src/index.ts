1e3b31137b463450e910be1c600e84e9758a2936
Víctor Velarde <victor.velarde@gmail.com>
2020-04-06 12:51:34 +0200

    feat: helper functions to create basemaps

--- packages/toolkit/src/index.ts
+++ packages/toolkit/src/index.ts
@@ -4,8 +4,13 @@ export { SQL } from '@carto/toolkit-sql';
 export { default as App } from './App';
 export { default as OAuthApp } from './OAuthApp';
 
-import { colorBinsStyle, colorCategoriesStyle, Layer } from '@carto/toolkit-viz';
-export const viz = { Layer, colorBinsStyle, colorCategoriesStyle };
+import { colorBinsStyle, colorCategoriesStyle, gmap, Layer, map } from '@carto/toolkit-viz';
+export const viz = {
+  map,
+  gmap,
+  Layer,
+  colorBinsStyle,
+  colorCategoriesStyle };
 
 // TODO: This should not be public
 export { CustomStorage } from '@carto/toolkit-custom-storage';
